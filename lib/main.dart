2f33ad1aa3c71a725cfe5b6de4d9b4ec53ddc422
ArGendi <73427247+ArGendi@users.noreply.github.com>
2021-08-17 23:17:14 +0200

    add new font and edit theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:transportation/constants.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Transportation',
-      theme: ThemeData(),
+      theme: ThemeData(
+          fontFamily: 'Changa',
+          appBarTheme: AppBarTheme(
+            color: primaryColor,
+          ),
+      ),
       home: Container(),
     );
   }
